a335d88991c7e8ef0504c5418faf19581fb2c405
Annastasiagubanova <anastasiagubanova.00@gmail.com>
2026-08-19 14:03:34 +0300

    c rus

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const formData = {
     tsdSubmitted: document.getElementById("tsdSubmitted")?.value || "",
     reason: document.getElementById("reason")?.value || "",
     tsdIssued: document.getElementById("tsdIssued")?.value || "",
-    сondition: document.getElementById("сondition")?.value || "",
+    сondition: document.getElementById("condition")?.value || "",
     worker1: worker1,
     worker2: worker2,
     pin1: pin1,
